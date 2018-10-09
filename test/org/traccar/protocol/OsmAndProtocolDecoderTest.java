81741c7a79a33c66e59ba3047c2269fc16287231
fermadeiral <fer.madeiral@gmail.com>
2018-10-09 22:59:30 +0200

    Bug commit from traccar/traccar This commit is based on the source code from the following commit: http://github.com/traccar/traccar/commit/e104f5636d53311dfd91cafaa2cdea65260ae7dc The mentioned commit triggered the following Travis build: http://travis-ci.org/traccar/traccar/builds/219945896.

---
+++ test/org/traccar/protocol/OsmAndProtocolDecoderTest.java
@@ -0,0 +1,42 @@
+package org.traccar.protocol;
+
+import org.junit.Test;
+import org.traccar.ProtocolTest;
+
+public class OsmAndProtocolDecoderTest extends ProtocolTest {
+
+    @Test
+    public void testDecode() throws Exception {
+
+        OsmAndProtocolDecoder decoder = new OsmAndProtocolDecoder(new OsmAndProtocol());
+
+        verifyNothing(decoder, request(
+                "/?timestamp=1377177267&lat=60.0&lon=30.0"));
+
+        verifyPosition(decoder, request(
+                "/?id=902064&lat=42.06288&lon=-88.23412&timestamp=2016-01-27T18%3A55%3A47Z&hdop=6.0&altitude=224.0&speed=0.0"));
+
+        verifyPosition(decoder, request(
+                "/?id=902064&lat=42.06288&lon=-88.23412&timestamp=1442068686579&hdop=6.0&altitude=224.0&speed=0.0"));
+
+        verifyPosition(decoder, request(
+                "/?lat=49.60688&lon=6.15788&timestamp=2014-06-04+09%3A10%3A11&altitude=384.7&speed=0.0&id=353861053849681"));
+        
+        verifyPosition(decoder, request(
+                "/?id=123456&timestamp=1377177267&lat=60.0&lon=30.0&speed=0.0&bearing=0.0&altitude=0&hdop=0.0"));
+        
+        verifyPosition(decoder, request(
+                "/?id=123456&timestamp=1377177267&lat=60.0&lon=30.0"));
+        
+        verifyPosition(decoder, request(
+                "/?lat=60.0&lon=30.0&speed=0.0&heading=0.0&vacc=0&hacc=0&altitude=0&deviceid=123456"));
+
+        verifyPosition(decoder, request(
+                "/?id=861001000719969&lat=41.666667&lon=-0.883333&altitude=350.059479&speed=0.000000&batt=87"));
+
+        verifyPosition(decoder, request(
+                "/?id=123456&timestamp=1377177267&location=60.0,30.0"));
+
+    }
+
+}
