2fbc8fb22fc6cd4c5d24df1c3f868cb9f60bec14
fermadeiral <fer.madeiral@gmail.com>
2018-10-09 22:58:39 +0200

    Bug commit from traccar/traccar This commit is based on the source code from the following commit: http://github.com/traccar/traccar/commit/2f46d48846633fbf56289f478a9acbe544a17c9c The mentioned commit triggered the following Travis build: http://travis-ci.org/traccar/traccar/builds/220525069.

---
+++ src/org/traccar/model/Device.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2012 - 2016 Anton Tananaev (anton@traccar.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Device extends Extensible {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String uniqueId;
+
+    public String getUniqueId() {
+        return uniqueId;
+    }
+
+    public void setUniqueId(String uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+
+    public static final String STATUS_UNKNOWN = "unknown";
+    public static final String STATUS_ONLINE = "online";
+    public static final String STATUS_OFFLINE = "offline";
+
+    private String status;
+
+    public String getStatus() {
+        return status != null ? status : STATUS_OFFLINE;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    private Date lastUpdate;
+
+    public Date getLastUpdate() {
+        if (lastUpdate != null) {
+            return new Date(lastUpdate.getTime());
+        } else {
+            return null;
+        }
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        if (lastUpdate != null) {
+            this.lastUpdate = new Date(lastUpdate.getTime());
+        } else {
+            this.lastUpdate = null;
+        }
+    }
+
+    private long positionId;
+
+    public long getPositionId() {
+        return positionId;
+    }
+
+    public void setPositionId(long positionId) {
+        this.positionId = positionId;
+    }
+
+    private long groupId;
+
+    public long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(long groupId) {
+        this.groupId = groupId;
+    }
+
+    private List<Long> geofenceIds;
+
+    public List<Long> getGeofenceIds() {
+        return geofenceIds;
+    }
+
+    public void setGeofenceIds(List<Long> geofenceIds) {
+        this.geofenceIds = geofenceIds;
+    }
+
+    private String phone;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    private String model;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    private String contact;
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    private String category;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+}
