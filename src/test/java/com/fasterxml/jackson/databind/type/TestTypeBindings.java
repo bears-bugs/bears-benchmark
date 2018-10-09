46b38bafb011f68c9a6c7724197ef8c8eaea9115
fermadeiral <fer.madeiral@gmail.com>
2018-10-09 23:32:32 +0200

    Bug commit from FasterXML/jackson-databind This commit is based on the source code from the following commit: http://github.com/FasterXML/jackson-databind/commit/28ec8a47d381311818f805ae76bf84da30f516f9 The mentioned commit triggered the following Travis build: http://travis-ci.org/FasterXML/jackson-databind/builds/221825856.

---
+++ src/test/java/com/fasterxml/jackson/databind/type/TestTypeBindings.java
@@ -0,0 +1,52 @@
+package com.fasterxml.jackson.databind.type;
+
+import java.util.*;
+
+import com.fasterxml.jackson.databind.BaseMapTest;
+import com.fasterxml.jackson.databind.JavaType;
+
+/**
+ * Simple tests to verify for generic type binding functionality
+ * implemented by {@link TypeBindings} class.
+ */
+public class TestTypeBindings
+    extends BaseMapTest
+{    
+    static class AbstractType<A,B> { }
+    
+    static class LongStringType extends AbstractType<Long,String> { }
+
+    static class InnerGenericTyping<K, V> extends AbstractMap<K, Collection<V>>
+    {
+        @Override
+        public Set<java.util.Map.Entry<K, Collection<V>>> entrySet() {
+            return null;
+        }
+        public class InnerClass extends AbstractMap<K, Collection<V>> {
+            @Override
+            public Set<java.util.Map.Entry<K, Collection<V>>> entrySet() {
+                return null;
+            }
+        }
+    }
+    
+    /*
+    /**********************************************************
+    /* Test methods
+    /**********************************************************
+     */
+
+    // [JACKSON-677]
+    public void testInnerType() throws Exception
+    {
+        TypeFactory tf = TypeFactory.defaultInstance();
+        JavaType type = tf.constructType(InnerGenericTyping.InnerClass.class);
+        assertEquals(MapType.class, type.getClass());
+        JavaType keyType = type.getKeyType();
+        assertEquals(Object.class, keyType.getRawClass());
+        JavaType valueType = type.getContentType();
+        assertEquals(Collection.class, valueType.getRawClass());
+        JavaType vt2 = valueType.getContentType();
+        assertEquals(Object.class, vt2.getRawClass());
+    }
+}
