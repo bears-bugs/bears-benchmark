750abc9d794ed6f34391ba7b70b95d3ea60f37bb
fermadeiral <fer.madeiral@gmail.com>
2018-10-10 06:34:15 +0200

    Bug commit from FasterXML/jackson-databind This commit is based on the source code from the following commit: http://github.com/FasterXML/jackson-databind/commit/16d77253d2ba0ed293a12013e459d56420a7d056 The mentioned commit triggered the following Travis build: http://travis-ci.org/FasterXML/jackson-databind/builds/195430379.

---
+++ src/test/java/com/fasterxml/jackson/databind/deser/TestNullHandling.java
@@ -0,0 +1,139 @@
+package com.fasterxml.jackson.databind.deser;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+public class TestNullHandling extends BaseMapTest
+{
+    static class FunnyNullDeserializer extends JsonDeserializer<String>
+    {
+        @Override
+        public String deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+            return "text";
+        }
+
+        @Override
+        public String getNullValue(DeserializationContext ctxt) { return "funny"; }
+    }
+
+    static class AnySetter{
+
+        private Map<String,String> any = new HashMap<String,String>();
+
+        @JsonAnySetter
+        public void setAny(String name, String value){
+            this.any.put(name,value);
+        }
+
+        public Map<String,String> getAny(){
+            return this.any;
+        }
+    }
+
+    /*
+    /**********************************************************
+    /* Test methods
+    /**********************************************************
+     */
+
+    public void testAnySetterNulls() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule("test", Version.unknownVersion());
+        module.addDeserializer(String.class, new FunnyNullDeserializer());
+        mapper.registerModule(module);
+
+        String fieldName = "fieldName";
+        String nullValue = "{\""+fieldName+"\":null}";
+
+        // should get non-default null directly:
+        AnySetter result = mapper.readValue(nullValue, AnySetter.class);
+
+        assertEquals(1, result.getAny().size());
+        assertNotNull(result.getAny().get(fieldName));
+        assertEquals("funny", result.getAny().get(fieldName));
+
+        // as well as via ObjectReader
+        ObjectReader reader = mapper.readerFor(AnySetter.class);
+        result = reader.readValue(nullValue);
+
+        assertEquals(1, result.getAny().size());
+        assertNotNull(result.getAny().get(fieldName));
+        assertEquals("funny", result.getAny().get(fieldName));
+    }
+
+    // Test for [JACKSON-643]
+    public void testCustomRootNulls() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule("test", Version.unknownVersion());
+        module.addDeserializer(String.class, new FunnyNullDeserializer());
+        mapper.registerModule(module);
+
+        // should get non-default null directly:
+        String str = mapper.readValue("null", String.class);
+        assertNotNull(str);
+        assertEquals("funny", str);
+        
+        // as well as via ObjectReader
+        ObjectReader reader = mapper.readerFor(String.class);
+        str = reader.readValue("null");
+        assertNotNull(str);
+        assertEquals("funny", str);
+    }
+
+    // Test for [#407]
+    public void testListOfNulls() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule("test", Version.unknownVersion());
+        module.addDeserializer(String.class, new FunnyNullDeserializer());
+        mapper.registerModule(module);
+
+        List<String> list = Arrays.asList("funny");
+        JavaType type = mapper.getTypeFactory().constructCollectionType(List.class, String.class);
+
+        // should get non-default null directly:
+        List<?> deser = mapper.readValue("[null]", type);
+        assertNotNull(deser);
+        assertEquals(1, deser.size());
+        assertEquals(list.get(0), deser.get(0));
+
+        // as well as via ObjectReader
+        ObjectReader reader = mapper.readerFor(type);
+        deser = reader.readValue("[null]");
+        assertNotNull(deser);
+        assertEquals(1, deser.size());
+        assertEquals(list.get(0), deser.get(0));
+    }
+
+    // Test for [#407]
+    public void testMapOfNulls() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule("test", Version.unknownVersion());
+        module.addDeserializer(String.class, new FunnyNullDeserializer());
+        mapper.registerModule(module);
+
+        JavaType type = mapper.getTypeFactory().constructMapType(Map.class, String.class, String.class);
+        // should get non-default null directly:
+        Map<?,?> deser = mapper.readValue("{\"key\":null}", type);
+        assertNotNull(deser);
+        assertEquals(1, deser.size());
+        assertEquals("funny", deser.get("key"));
+
+        // as well as via ObjectReader
+        ObjectReader reader = mapper.readerFor(type);
+        deser = reader.readValue("{\"key\":null}");
+        assertNotNull(deser);
+        assertEquals(1, deser.size());
+        assertEquals("funny", deser.get("key"));
+    }
+}
---
+++ src/test/java/com/fasterxml/jackson/databind/ser/TestCollectionSerialization.java
@@ -0,0 +1,253 @@
+package com.fasterxml.jackson.databind.ser;
+
+import java.io.*;
+import java.util.*;
+
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+public class TestCollectionSerialization
+    extends BaseMapTest
+{
+    enum Key { A, B, C };
+
+    // Field-based simple bean with a single property, "values"
+    final static class CollectionBean
+    {
+        @JsonProperty // not required
+        public Collection<Object> values;
+
+        public CollectionBean(Collection<Object> c) { values = c; }
+    }
+
+    static class EnumMapBean
+    {
+        EnumMap<Key,String> _map;
+
+        public EnumMapBean(EnumMap<Key,String> m)
+        {
+            _map = m;
+        }
+
+        public EnumMap<Key,String> getMap() { return _map; }
+    }
+
+    /**
+     * Class needed for testing [JACKSON-220]
+     */
+    @SuppressWarnings("serial")
+    @JsonSerialize(using=ListSerializer.class)    
+    static class PseudoList extends ArrayList<String>
+    {
+        public PseudoList(String... values) {
+            super(Arrays.asList(values));
+        }
+    }
+
+    static class ListSerializer extends JsonSerializer<List<String>>
+    {
+        @Override
+        public void serialize(List<String> value, JsonGenerator gen, SerializerProvider provider)
+            throws IOException
+        {
+            // just use standard List.toString(), output as JSON String
+            gen.writeString(value.toString());
+        }
+    }
+
+    // for [JACKSON-254], suppression of empty collections
+    static class EmptyListBean {
+        public List<String> empty = new ArrayList<String>();
+    }
+
+    static class EmptyArrayBean {
+        public String[] empty = new String[0];
+    }
+
+    /*
+    /**********************************************************
+    /* Test methods
+    /**********************************************************
+     */
+
+    private final static ObjectMapper MAPPER = new ObjectMapper();
+
+    public void testCollections() throws IOException
+    {
+        // Let's try different collections, arrays etc
+        final int entryLen = 98;
+
+        for (int type = 0; type < 4; ++type) {
+            Object value;
+
+            if (type == 0) { // first, array
+                int[] ints = new int[entryLen];
+                for (int i = 0; i < entryLen; ++i) {
+                    ints[i] = Integer.valueOf(i);
+                }
+                value = ints;
+            } else {
+                Collection<Integer> c;
+
+                switch (type) {
+                case 1:
+                    c = new LinkedList<Integer>();
+                    break;
+                case 2:
+                    c = new TreeSet<Integer>(); // has to be ordered
+                    break;
+                default:
+                    c = new ArrayList<Integer>();
+                    break;
+                }
+                for (int i = 0; i < entryLen; ++i) {
+                    c.add(Integer.valueOf(i));
+                }
+                value = c;
+            }
+            String json = MAPPER.writeValueAsString(value);
+            
+            // and then need to verify:
+            JsonParser jp = new JsonFactory().createParser(json);
+            assertToken(JsonToken.START_ARRAY, jp.nextToken());
+            for (int i = 0; i < entryLen; ++i) {
+                assertToken(JsonToken.VALUE_NUMBER_INT, jp.nextToken());
+                assertEquals(i, jp.getIntValue());
+            }
+            assertToken(JsonToken.END_ARRAY, jp.nextToken());
+            jp.close();
+        }
+    }
+
+    @SuppressWarnings("resource")
+    public void testBigCollection() throws IOException
+    {
+        final int COUNT = 9999;
+        ArrayList<Integer> value = new ArrayList<Integer>();
+        for (int i = 0; i <= COUNT; ++i) {
+            value.add(i);
+        }
+        // Let's test using 3 main variants...
+        for (int mode = 0; mode < 3; ++mode) {
+            JsonParser jp = null;
+            switch (mode) {
+            case 0:
+                {
+                    byte[] data = MAPPER.writeValueAsBytes(value);
+                    jp = new JsonFactory().createParser(data);
+                }
+                break;
+            case 1:
+                {
+                    StringWriter sw = new StringWriter(value.size());
+                    MAPPER.writeValue(sw, value);
+                    jp = createParserUsingReader(sw.toString());
+                }
+                break;
+            case 2:
+                {
+                    String str = MAPPER.writeValueAsString(value);
+                    jp = createParserUsingReader(str);
+                }
+                break;
+            }
+
+            // and verify
+            assertToken(JsonToken.START_ARRAY, jp.nextToken());
+            for (int i = 0; i <= COUNT; ++i) {
+                assertEquals(JsonToken.VALUE_NUMBER_INT, jp.nextToken());
+                assertEquals(i, jp.getIntValue());
+            }
+            assertToken(JsonToken.END_ARRAY, jp.nextToken());
+            jp.close();
+        }
+    }
+
+    public void testEnumMap() throws IOException
+    {
+        EnumMap<Key,String> map = new EnumMap<Key,String>(Key.class);
+        map.put(Key.B, "xyz");
+        map.put(Key.C, "abc");
+        // assuming EnumMap uses enum entry order, which I think is true...
+        String json = MAPPER.writeValueAsString(map);
+        assertEquals("{\"B\":\"xyz\",\"C\":\"abc\"}",json.trim());
+    }
+
+    // Test that checks that empty collections are properly serialized
+    // when they are Bean properties
+    @SuppressWarnings("unchecked")
+    public void testEmptyBeanCollection() throws IOException
+    {
+        Collection<Object> x = new ArrayList<Object>();
+        x.add("foobar");
+        CollectionBean cb = new CollectionBean(x);
+        Map<String,Object> result = writeAndMap(MAPPER, cb);
+        assertEquals(1, result.size());
+        assertTrue(result.containsKey("values"));
+        Collection<Object> x2 = (Collection<Object>) result.get("values");
+        assertNotNull(x2);
+        assertEquals(x, x2);
+    }
+
+    public void testNullBeanCollection()
+        throws IOException
+    {
+        CollectionBean cb = new CollectionBean(null);
+        Map<String,Object> result = writeAndMap(MAPPER, cb);
+        assertEquals(1, result.size());
+        assertTrue(result.containsKey("values"));
+        assertNull(result.get("values"));
+    }
+
+    @SuppressWarnings("unchecked")
+    public void testEmptyBeanEnumMap() throws IOException
+    {
+        EnumMap<Key,String> map = new EnumMap<Key,String>(Key.class);
+        EnumMapBean b = new EnumMapBean(map);
+        Map<String,Object> result = writeAndMap(MAPPER, b);
+
+        assertEquals(1, result.size());
+        assertTrue(result.containsKey("map"));
+        // we deserialized to untyped, not back to bean, so:
+        Map<Object,Object> map2 = (Map<Object,Object>) result.get("map");
+        assertNotNull(map2);
+        assertEquals(0, map2.size());
+    }
+
+    // Should also be able to serialize null EnumMaps as expected
+    public void testNullBeanEnumMap() throws IOException
+    {
+        EnumMapBean b = new EnumMapBean(null);
+        Map<String,Object> result = writeAndMap(MAPPER, b);
+
+        assertEquals(1, result.size());
+        assertTrue(result.containsKey("map"));
+        assertNull(result.get("map"));
+    }
+
+    // Test [JACKSON-220]
+    public void testListSerializer() throws IOException
+    {
+        assertEquals("\"[ab, cd, ef]\"",
+                MAPPER.writeValueAsString(new PseudoList("ab", "cd", "ef")));
+    }
+
+    // [JACKSON-254]
+    public void testEmptyListOrArray() throws IOException
+    {
+        // by default, empty lists serialized normally
+        EmptyListBean list = new EmptyListBean();
+        EmptyArrayBean array = new EmptyArrayBean();
+        assertTrue(MAPPER.isEnabled(SerializationFeature.WRITE_EMPTY_JSON_ARRAYS));
+        assertEquals("{\"empty\":[]}", MAPPER.writeValueAsString(list));
+        assertEquals("{\"empty\":[]}", MAPPER.writeValueAsString(array));
+
+        // note: value of setting may be cached when constructing serializer, need a new instance
+        ObjectMapper m = new ObjectMapper();
+        m.configure(SerializationFeature.WRITE_EMPTY_JSON_ARRAYS, false);
+        assertEquals("{}", m.writeValueAsString(list));
+        assertEquals("{}", m.writeValueAsString(array));
+    }
+}
