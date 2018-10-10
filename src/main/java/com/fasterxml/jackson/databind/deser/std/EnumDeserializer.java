c5fe3738ca9f18b02c8e453fc1670bd746c0d284
fermadeiral <fer.madeiral@gmail.com>
2018-10-10 06:25:20 +0200

    Bug commit from FasterXML/jackson-databind This commit is based on the source code from the following commit: http://github.com/FasterXML/jackson-databind/commit/ce37efcefbfab0fa91562a492f0f80af018a5313 The mentioned commit triggered the following Travis build: http://travis-ci.org/FasterXML/jackson-databind/builds/203649003.

---
+++ src/main/java/com/fasterxml/jackson/databind/deser/std/EnumDeserializer.java
@@ -0,0 +1,230 @@
+package com.fasterxml.jackson.databind.deser.std;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.annotation.JacksonStdImpl;
+import com.fasterxml.jackson.databind.deser.SettableBeanProperty;
+import com.fasterxml.jackson.databind.deser.ValueInstantiator;
+import com.fasterxml.jackson.databind.introspect.AnnotatedMethod;
+import com.fasterxml.jackson.databind.util.ClassUtil;
+import com.fasterxml.jackson.databind.util.CompactStringObjectMap;
+import com.fasterxml.jackson.databind.util.EnumResolver;
+
+/**
+ * Deserializer class that can deserialize instances of
+ * specified Enum class from Strings and Integers.
+ */
+@JacksonStdImpl // was missing until 2.6
+public class EnumDeserializer
+    extends StdScalarDeserializer<Object>
+{
+    private static final long serialVersionUID = 1L;
+
+    protected Object[] _enumsByIndex;
+    
+    /**
+     * @since 2.8
+     */
+    private final Enum<?> _enumDefaultValue;
+
+    /**
+     * @since 2.7.3
+     */
+    protected final CompactStringObjectMap _lookupByName;
+
+    /**
+     * Alternatively, we may need a different lookup object if "use toString"
+     * is defined.
+     *
+     * @since 2.7.3
+     */
+    protected CompactStringObjectMap _lookupByToString;
+    
+    public EnumDeserializer(EnumResolver byNameResolver)
+    {
+        super(byNameResolver.getEnumClass());
+        _lookupByName = byNameResolver.constructLookup();
+        _enumsByIndex = byNameResolver.getRawEnums();
+        _enumDefaultValue = byNameResolver.getDefaultValue();
+    }
+
+    /**
+     * @deprecated Since 2.8
+     */
+    @Deprecated
+    public static JsonDeserializer<?> deserializerForCreator(DeserializationConfig config,
+            Class<?> enumClass, AnnotatedMethod factory) {
+        return deserializerForCreator(config, enumClass, factory, null, null);
+    }
+
+    /**
+     * Factory method used when Enum instances are to be deserialized
+     * using a creator (static factory method)
+     * 
+     * @return Deserializer based on given factory method
+     *
+     * @since 2.8
+     */
+    public static JsonDeserializer<?> deserializerForCreator(DeserializationConfig config,
+            Class<?> enumClass, AnnotatedMethod factory,
+            ValueInstantiator valueInstantiator, SettableBeanProperty[] creatorProps)
+    {
+        if (config.canOverrideAccessModifiers()) {
+            ClassUtil.checkAndFixAccess(factory.getMember(),
+                    config.isEnabled(MapperFeature.OVERRIDE_PUBLIC_ACCESS_MODIFIERS));
+        }
+        return new FactoryBasedEnumDeserializer(enumClass, factory,
+                factory.getParameterType(0),
+                valueInstantiator, creatorProps);
+    }
+
+    /**
+     * Factory method used when Enum instances are to be deserialized
+     * using a zero-/no-args factory method
+     * 
+     * @return Deserializer based on given no-args factory method
+     *
+     * @since 2.8
+     */
+    public static JsonDeserializer<?> deserializerForNoArgsCreator(DeserializationConfig config,
+            Class<?> enumClass, AnnotatedMethod factory)
+    {
+        if (config.canOverrideAccessModifiers()) {
+            ClassUtil.checkAndFixAccess(factory.getMember(),
+                    config.isEnabled(MapperFeature.OVERRIDE_PUBLIC_ACCESS_MODIFIERS));
+        }
+        return new FactoryBasedEnumDeserializer(enumClass, factory);
+    }
+
+    /*
+    /**********************************************************
+    /* Default JsonDeserializer implementation
+    /**********************************************************
+     */
+
+    /**
+     * Because of costs associated with constructing Enum resolvers,
+     * let's cache instances by default.
+     */
+    @Override
+    public boolean isCachable() { return true; }
+
+    @Override
+    public Object deserialize(JsonParser p, DeserializationContext ctxt) throws IOException
+    {
+        JsonToken curr = p.getCurrentToken();
+        
+        // Usually should just get string value:
+        if (curr == JsonToken.VALUE_STRING || curr == JsonToken.FIELD_NAME) {
+            CompactStringObjectMap lookup = ctxt.isEnabled(DeserializationFeature.READ_ENUMS_USING_TO_STRING)
+                    ? _getToStringLookup(ctxt) : _lookupByName;
+            final String name = p.getText();
+            Object result = lookup.find(name);
+            if (result == null) {
+                return _deserializeAltString(p, ctxt, lookup, name);
+            }
+            return result;
+        }
+        // But let's consider int acceptable as well (if within ordinal range)
+        if (curr == JsonToken.VALUE_NUMBER_INT) {
+            // ... unless told not to do that
+            int index = p.getIntValue();
+            if (ctxt.isEnabled(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS)) {
+                return ctxt.handleWeirdNumberValue(_enumClass(), index,
+                        "not allowed to deserialize Enum value out of number: disable DeserializationConfig.DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS to allow"
+                        );
+            }
+            if (index >= 0 && index < _enumsByIndex.length) {
+                return _enumsByIndex[index];
+            }
+            if ((_enumDefaultValue != null)
+                    && ctxt.isEnabled(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE)) {
+                return _enumDefaultValue;
+            }
+            if (!ctxt.isEnabled(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL)) {
+                return ctxt.handleWeirdNumberValue(_enumClass(), index,
+                        "index value outside legal index range [0..%s]",
+                        _enumsByIndex.length-1);
+            }
+            return null;
+        }
+        return _deserializeOther(p, ctxt);
+    }
+
+    /*
+    /**********************************************************
+    /* Internal helper methods
+    /**********************************************************
+     */
+    
+    private final Object _deserializeAltString(JsonParser p, DeserializationContext ctxt,
+            CompactStringObjectMap lookup, String name) throws IOException
+    {
+        name = name.trim();
+        if (name.length() == 0) {
+            if (ctxt.isEnabled(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)) {
+                return null;
+            }
+        } else if (!ctxt.isEnabled(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS)) {
+            // [databind#149]: Allow use of 'String' indexes as well -- unless prohibited (as per above)
+            char c = name.charAt(0);
+            if (c >= '0' && c <= '9') {
+                try {
+                    int index = Integer.parseInt(name);
+                    if (index >= 0 && index < _enumsByIndex.length) {
+                        return _enumsByIndex[index];
+                    }
+                } catch (NumberFormatException e) {
+                    // fine, ignore, was not an integer
+                }
+            }
+        }
+        if ((_enumDefaultValue != null)
+                && ctxt.isEnabled(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE)) {
+            return _enumDefaultValue;
+        }
+        if (!ctxt.isEnabled(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL)) {
+            return ctxt.handleWeirdStringValue(_enumClass(), name,
+                    "value not one of declared Enum instance names: %s", lookup.keys());
+        }
+        return null;
+    }
+
+    protected Object _deserializeOther(JsonParser p, DeserializationContext ctxt) throws IOException
+    {
+        // [databind#381]
+        if (ctxt.isEnabled(DeserializationFeature.UNWRAP_SINGLE_VALUE_ARRAYS)
+                && p.isExpectedStartArrayToken()) {
+            p.nextToken();
+            final Object parsed = deserialize(p, ctxt);
+            JsonToken curr = p.nextToken();
+            if (curr != JsonToken.END_ARRAY) {
+                handleMissingEndArrayForSingle(p, ctxt);
+            }
+            return parsed;
+        }
+        return ctxt.handleUnexpectedToken(_enumClass(), p);
+    }
+
+    protected Class<?> _enumClass() {
+        return handledType();
+    }
+
+    protected CompactStringObjectMap _getToStringLookup(DeserializationContext ctxt)
+    {
+        CompactStringObjectMap lookup = _lookupByToString;
+        // note: exact locking not needed; all we care for here is to try to
+        // reduce contention for the initial resolution
+        if (lookup == null) {
+            synchronized (this) {
+                lookup = EnumResolver.constructUnsafeUsingToString(_enumClass(),
+                        ctxt.getAnnotationIntrospector())
+                    .constructLookup();
+            }
+            _lookupByToString = lookup;
+        }
+        return lookup;
+    }
+}
---
+++ src/test/java/com/fasterxml/jackson/databind/ser/NumberSerTest.java
@@ -0,0 +1,90 @@
+package com.fasterxml.jackson.databind.ser;
+
+import java.math.BigInteger;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.BaseMapTest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Unit tests for verifying serialization of simple basic non-structured
+ * types; primitives (and/or their wrappers), Strings.
+ */
+public class NumberSerTest extends BaseMapTest
+{
+    private final ObjectMapper MAPPER = objectMapper();
+
+    static class IntWrapper {
+        public int i;
+
+        public IntWrapper(int value) { i = value; }
+    }
+    
+    static class IntAsString {
+        @JsonFormat(shape=JsonFormat.Shape.STRING)
+        @JsonProperty("value")
+        public int foo = 3;
+    }
+
+    static class LongAsString {
+        @JsonFormat(shape=JsonFormat.Shape.STRING)
+        public long value = 4;
+    }
+
+    static class DoubleAsString {
+        @JsonFormat(shape=JsonFormat.Shape.STRING)
+        public double value = -0.5;
+    }
+
+    /*
+    /**********************************************************
+    /* Test methods
+    /**********************************************************
+     */
+
+    public void testDouble() throws Exception
+    {
+        double[] values = new double[] {
+            0.0, 1.0, 0.1, -37.01, 999.99, 0.3, 33.3, Double.NaN, Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY
+        };
+        for (double d : values) {
+            String expected = String.valueOf(d);
+            if (Double.isNaN(d) || Double.isInfinite(d)) {
+                expected = "\""+d+"\"";
+            }
+            assertEquals(expected, MAPPER.writeValueAsString(Double.valueOf(d)));
+        }
+    }
+
+    public void testBigInteger() throws Exception
+    {
+        BigInteger[] values = new BigInteger[] {
+                BigInteger.ONE, BigInteger.TEN, BigInteger.ZERO,
+                BigInteger.valueOf(1234567890L),
+                new BigInteger("123456789012345678901234568"),
+                new BigInteger("-1250000124326904597090347547457")
+                };
+
+        for (BigInteger value : values) {
+            String expected = value.toString();
+            assertEquals(expected, MAPPER.writeValueAsString(value));
+        }
+    }
+
+    public void testNumbersAsString() throws Exception
+    {
+        assertEquals(aposToQuotes("{'value':'3'}"), MAPPER.writeValueAsString(new IntAsString()));
+        assertEquals(aposToQuotes("{'value':'4'}"), MAPPER.writeValueAsString(new LongAsString()));
+        assertEquals(aposToQuotes("{'value':'-0.5'}"), MAPPER.writeValueAsString(new DoubleAsString()));
+    }
+
+    public void testConfigOverridesForNumbers() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configOverride(Integer.TYPE) // for `int`
+            .setFormat(JsonFormat.Value.forShape(JsonFormat.Shape.STRING));
+        assertEquals(aposToQuotes("{'i':'3'}"),
+                mapper.writeValueAsString(new IntWrapper(3)));
+    }
+}
