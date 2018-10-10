cdc0c96c54936b32e379388d4f4fff6ecdd2e02c
fermadeiral <fer.madeiral@gmail.com>
2018-10-10 04:31:35 +0200

    Bug commit from traccar/traccar This commit is based on the source code from the following commit: http://github.com/traccar/traccar/commit/d344bd8061dd15b0613a7f628decbd8bca3df095 The mentioned commit triggered the following Travis build: http://travis-ci.org/traccar/traccar/builds/216189551.

---
+++ src/org/traccar/protocol/Pt502ProtocolDecoder.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2012 - 2016 Anton Tananaev (anton@traccar.org)
+ * Copyright 2012 Luis Parada (luis.parada@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.protocol;
+
+import org.jboss.netty.channel.Channel;
+import org.traccar.BaseProtocolDecoder;
+import org.traccar.DeviceSession;
+import org.traccar.helper.DateBuilder;
+import org.traccar.helper.Parser;
+import org.traccar.helper.PatternBuilder;
+import org.traccar.model.Position;
+
+import java.net.SocketAddress;
+import java.util.regex.Pattern;
+
+public class Pt502ProtocolDecoder extends BaseProtocolDecoder {
+
+    private static final int MAX_CHUNK_SIZE = 960;
+
+    private byte[] photo;
+
+    public Pt502ProtocolDecoder(Pt502Protocol protocol) {
+        super(protocol);
+    }
+
+    private static final Pattern PATTERN = new PatternBuilder()
+            .any().text("$")
+            .expression("([^,]+),")              // type
+            .number("(d+),")                     // id
+            .number("(dd)(dd)(dd).(ddd),")       // time (hhmmss.sss)
+            .expression("([AV]),")               // validity
+            .number("(dd)(dd.dddd),")            // latitude
+            .expression("([NS]),")
+            .number("(ddd)(dd.dddd),")           // longitude
+            .expression("([EW]),")
+            .number("(d+.d+)?,")                 // speed
+            .number("(d+.d+)?,")                 // course
+            .number("(dd)(dd)(dd),,,")           // date (ddmmyy)
+            .expression("./")
+            .expression("([01])+,")              // input
+            .expression("([01])+/")              // output
+            .expression("([^/]+)?/")             // adc
+            .number("(d+)")                      // odometer
+            .expression("/([^/]+)?/")            // rfid
+            .number("(xxx)").optional(2)         // state
+            .any()
+            .compile();
+
+    private String decodeAlarm(String value) {
+        switch (value) {
+            case "TOW":
+                return Position.ALARM_TOW;
+            case "HDA":
+                return Position.ALARM_ACCELERATION;
+            case "HDB":
+                return Position.ALARM_BREAKING;
+            case "FDA":
+                return Position.ALARM_FATIGUE_DRIVING;
+            case "SKA":
+                return Position.ALARM_VIBRATION;
+            case "PMA":
+                return Position.ALARM_MOVEMENT;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    protected Object decode(
+            Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
+
+        String sentence = (String) msg;
+
+        Parser parser = new Parser(PATTERN, sentence);
+        if (!parser.matches()) {
+            return null;
+        }
+
+        Position position = new Position();
+        position.setProtocol(getProtocolName());
+
+        String type = parser.next();
+
+        if (type.startsWith("PHO") && channel != null) {
+            photo = new byte[Integer.parseInt(type.substring(3))];
+            channel.write("#PHD0," + Math.min(photo.length, MAX_CHUNK_SIZE) + "\r\n");
+        }
+
+        position.set(Position.KEY_ALARM, decodeAlarm(type));
+
+        DeviceSession deviceSession = getDeviceSession(channel, remoteAddress, parser.next());
+        if (deviceSession == null) {
+            return null;
+        }
+        position.setDeviceId(deviceSession.getDeviceId());
+
+        DateBuilder dateBuilder = new DateBuilder()
+                .setTime(parser.nextInt(), parser.nextInt(), parser.nextInt(), parser.nextInt());
+
+        position.setValid(parser.next().equals("A"));
+        position.setLatitude(parser.nextCoordinate());
+        position.setLongitude(parser.nextCoordinate());
+        position.setSpeed(parser.nextDouble());
+        position.setCourse(parser.nextDouble());
+
+        dateBuilder.setDateReverse(parser.nextInt(), parser.nextInt(), parser.nextInt());
+        position.setTime(dateBuilder.getDate());
+
+        position.set(Position.KEY_INPUT, parser.next());
+        position.set(Position.KEY_OUTPUT, parser.next());
+
+        if (parser.hasNext()) {
+            String[] values = parser.next().split(",");
+            for (int i = 0; i < values.length; i++) {
+                position.set(Position.PREFIX_ADC + (i + 1), Integer.parseInt(values[i], 16));
+            }
+        }
+
+        position.set(Position.KEY_ODOMETER, parser.nextInt());
+        position.set(Position.KEY_RFID, parser.next());
+
+        if (parser.hasNext()) {
+            int value = parser.nextInt(16);
+            position.set(Position.KEY_BATTERY, value >> 8);
+            position.set(Position.KEY_RSSI, (value >> 4) & 0xf);
+            position.set(Position.KEY_SATELLITES, value & 0xf);
+        }
+
+        return position;
+    }
+
+}
