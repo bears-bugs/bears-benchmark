dcd255b6f87f17fb89ab6dc8a172d00025b421f3
fermadeiral <fer.madeiral@gmail.com>
2018-10-10 16:06:02 +0200

    Bug commit from INRIA/spoon This commit is based on the source code from the following commit: http://github.com/INRIA/spoon/commit/54e1909ab7217cd8417d4d15cc7c0d127750b474 The mentioned commit triggered the following Travis build: http://travis-ci.org/INRIA/spoon/builds/237078306.

---
+++ src/test/java/spoon/test/imports/ImportScannerTest.java
@@ -0,0 +1,97 @@
+package spoon.test.imports;
+
+import org.junit.Test;
+import spoon.Launcher;
+import spoon.SpoonModelBuilder;
+import spoon.compiler.SpoonResourceHelper;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.factory.Factory;
+import spoon.reflect.reference.CtTypeReference;
+import spoon.reflect.visitor.ImportScanner;
+import spoon.reflect.visitor.ImportScannerImpl;
+import spoon.reflect.visitor.MinimalImportScanner;
+import spoon.reflect.visitor.Query;
+import spoon.reflect.visitor.filter.NameFilter;
+
+import java.security.AccessControlException;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static spoon.testing.utils.ModelUtils.build;
+
+/**
+ * Created by gerard on 14/10/2014.
+ */
+public class ImportScannerTest {
+
+	@Test
+	public void testComputeMinimalImportsInClass() throws Exception {
+		String packageName = "spoon.test";
+		String className = "SampleImportClass";
+		String qualifiedName = packageName + "." + className;
+
+		Factory aFactory = build(packageName, className).getFactory();
+		CtType<?> theClass = aFactory.Type().get(qualifiedName);
+
+		ImportScanner importContext = new MinimalImportScanner();
+		Collection<CtTypeReference<?>> imports = importContext.computeImports(theClass);
+
+		assertTrue(imports.isEmpty());
+	}
+
+	@Test
+	public void testComputeImportsInClass() throws Exception {
+		String packageName = "spoon.test";
+		String className = "SampleImportClass";
+		String qualifiedName = packageName + "." + className;
+
+		Factory aFactory = build(packageName, className).getFactory();
+		CtType<?> theClass = aFactory.Type().get(qualifiedName);
+
+		ImportScanner importContext = new ImportScannerImpl();
+		Collection<CtTypeReference<?>> imports = importContext.computeImports(theClass);
+
+		// java.lang are also computed
+		assertEquals(4, imports.size());
+	}
+
+	@Test
+	public void testComputeImportsInClassWithSameName() throws Exception {
+		String packageName = "spoon.test.imports.testclasses2";
+		String className = "ImportSameName";
+		String qualifiedName = packageName + "." + className;
+
+		Factory aFactory = build(packageName, className).getFactory();
+		CtType<?> theClass = aFactory.Type().get(qualifiedName);
+
+		ImportScanner importContext = new ImportScannerImpl();
+		Collection<CtTypeReference<?>> imports = importContext.computeImports(theClass);
+
+		assertEquals(0, imports.size());
+	}
+
+
+	@Test
+	public void testMultiCatchImport() throws Exception {
+		Launcher spoon = new Launcher();
+		Factory factory = spoon.createFactory();
+
+		SpoonModelBuilder compiler = spoon.createCompiler(
+				factory,
+				SpoonResourceHelper.resources(
+						"./src/test/java/spoon/test/imports/testclasses/MultiCatch.java"));
+
+		compiler.build();
+
+		final List<CtClass<?>> classes = Query.getElements(factory, new NameFilter<CtClass<?>>("MultiCatch"));
+
+		ImportScanner importScanner = new ImportScannerImpl();
+		importScanner.computeImports(classes.get(0));
+		// as ArithmeticException come from java.lang it is not imported anymore
+		//assertTrue( importScanner.isImported( factory.Type().createReference( ArithmeticException.class ) ));
+		assertTrue( importScanner.isImported( factory.Type().createReference( AccessControlException.class ) ));
+	}
+}
---
+++ src/test/java/spoon/test/prettyprinter/PrinterTest.java
@@ -0,0 +1,140 @@
+package spoon.test.prettyprinter;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static spoon.testing.utils.ModelUtils.canBeBuilt;
+
+import org.junit.Test;
+
+import spoon.Launcher;
+import spoon.compiler.SpoonResourceHelper;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.factory.Factory;
+import spoon.reflect.visitor.PrettyPrinter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrinterTest {
+
+	@Test
+	public void testPrettyPrinter() throws Exception {
+		Launcher spoon = new Launcher();
+		Factory factory = spoon.createFactory();
+		spoon.createCompiler(
+				factory,
+				SpoonResourceHelper
+						.resources(
+								"./src/test/java/spoon/test/annotation/testclasses/PersistenceProperty.java",
+								"./src/test/java/spoon/test/prettyprinter/Validation.java"))
+				.build();
+		for (CtType<?> t : factory.Type().getAll()) {
+			t.toString();
+		}
+		assertEquals(0, factory.getEnvironment().getWarningCount());
+		assertEquals(0, factory.getEnvironment().getErrorCount());
+
+	}
+
+	@Test
+	public void testChangeAutoImportModeWorks() throws Exception {
+		Launcher spoon = new Launcher();
+		PrettyPrinter printer = spoon.createPrettyPrinter();
+		spoon.getEnvironment().setAutoImports(false);
+		spoon.addInputResource("./src/test/java/spoon/test/prettyprinter/testclasses/AClass.java");
+		spoon.buildModel();
+
+		CtType element = spoon.getFactory().Class().getAll().get(0);
+		List<CtType<?>> toPrint = new ArrayList<>();
+		toPrint.add(element);
+		printer.calculate(element.getPosition().getCompilationUnit(), toPrint);
+		String result = printer.getResult();
+
+		assertTrue("The result should not contain imports: "+result, !result.contains("import java.util.List;"));
+
+		spoon.getEnvironment().setAutoImports(true);
+		printer.calculate(element.getPosition().getCompilationUnit(), toPrint);
+		result = printer.getResult();
+		assertTrue("The result should now contain imports: "+result, result.contains("import java.util.List;"));
+	}
+
+	@Test
+	public void testFQNModeWriteFQNConstructorInCtVisitor() {
+		Launcher spoon = new Launcher();
+		PrettyPrinter printer = spoon.createPrettyPrinter();
+		spoon.getEnvironment().setAutoImports(false);
+		spoon.addInputResource("./src/main/java/spoon/support/visitor/replace/ReplacementVisitor.java");
+		spoon.buildModel();
+
+		CtType element = spoon.getFactory().Class().getAll().get(0);
+		List<CtType<?>> toPrint = new ArrayList<>();
+		toPrint.add(element);
+		printer.calculate(element.getPosition().getCompilationUnit(), toPrint);
+		String result = printer.getResult();
+
+		assertTrue("The result should contain FQN for constructor: "+result, result.contains("new spoon.support.visitor.replace.ReplacementVisitor("));
+		assertTrue("The result should not contain reduced constructors: "+result, !result.contains("new ReplacementVisitor("));
+	}
+
+	@Test
+	public void testAutoimportModeDontImportUselessStatic() {
+		Launcher spoon = new Launcher();
+		PrettyPrinter printer = spoon.createPrettyPrinter();
+		spoon.getEnvironment().setAutoImports(true);
+		spoon.addInputResource("./src/test/java/spoon/test/prettyprinter/testclasses/ImportStatic.java");
+		spoon.buildModel();
+
+		CtType element = spoon.getFactory().Class().getAll().get(0);
+		List<CtType<?>> toPrint = new ArrayList<>();
+		toPrint.add(element);
+		printer.calculate(element.getPosition().getCompilationUnit(), toPrint);
+		String result = printer.getResult();
+
+		assertTrue("The result should not contain import static: ", !result.contains("import static spoon.test.prettyprinter.testclasses.sub.Constants.READY"));
+		assertTrue("The result should contain import type: ", result.contains("import spoon.test.prettyprinter.testclasses.sub.Constants"));
+		assertTrue("The result should contain import for TestCase: ", result.contains("import junit.framework.TestCase;"));
+		assertTrue("The result should contain assertTrue(...): ", result.contains("TestCase.assertTrue(\"blabla\".equals(\"toto\"));"));
+		assertTrue("The result should use System.out.println(Constants.READY): "+result, result.contains("System.out.println(Constants.READY);"));
+	}
+
+	@Test
+	public void testRuleCanBeBuild() {
+		Launcher spoon = new Launcher();
+		PrettyPrinter printer = spoon.createPrettyPrinter();
+		spoon.getEnvironment().setAutoImports(true);
+		String output = "./target/spoon-rule/";
+		spoon.addInputResource("./src/test/java/spoon/test/prettyprinter/testclasses/Rule.java");
+		spoon.setSourceOutputDirectory(output);
+		spoon.run();
+
+		CtType element = spoon.getFactory().Class().getAll().get(0);
+		List<CtType<?>> toPrint = new ArrayList<>();
+		toPrint.add(element);
+		printer.calculate(element.getPosition().getCompilationUnit(), toPrint);
+		String result = printer.getResult();
+
+		assertTrue("The result should contain direct this accessor for field: "+result, !result.contains("Rule.Phoneme.this.phonemeText"));
+		canBeBuilt(output, 7);
+	}
+
+	@Test
+	public void testJDTBatchCompilerCanBeBuild() {
+		Launcher spoon = new Launcher();
+		PrettyPrinter printer = spoon.createPrettyPrinter();
+		spoon.getEnvironment().setAutoImports(false);
+		String output = "./target/spoon-jdtbatchcompiler/";
+		spoon.addInputResource("./src/main/java/spoon/support/compiler/jdt/JDTBatchCompiler.java");
+		spoon.setSourceOutputDirectory(output);
+		spoon.run();
+
+		CtType element = spoon.getFactory().Class().getAll().get(0);
+		List<CtType<?>> toPrint = new ArrayList<>();
+		toPrint.add(element);
+		printer.calculate(element.getPosition().getCompilationUnit(), toPrint);
+		String result = printer.getResult();
+
+		//assertTrue("The result should contain direct this accessor for field: "+result, !result.contains("Rule.Phoneme.this.phonemeText"));
+		canBeBuilt(output, 7);
+	}
+
+}
