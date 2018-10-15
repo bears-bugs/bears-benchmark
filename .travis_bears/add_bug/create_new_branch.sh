321073a0070e240b08c98b6470e600253435e944
Fer Madeiral <fer.madeiral@gmail.com>
2018-10-15 00:32:13 -0300

    Initial commit

---
+++ .travis_bears/add_bug/create_new_branch.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+# the branch pr-add-bug is checked out
+
+BUGGY_COMMIT_ID=$(git log --format=format:%H --grep="$BUGGY_COMMIT_MESSAGE_PATTERN")
+TEST_COMMIT_ID=$(git log --format=format:%H --grep="$TEST_COMMIT_MESSAGE_PATTERN")
+PATCHED_COMMIT_ID=$(git log --format=format:%H --grep="$PATCHED_COMMIT_MESSAGE_PATTERN")
+END_COMMIT_ID=$(git log --format=format:%H --grep="$END_COMMIT_MESSAGE_PATTERN")
+
+# creates new orphan branch for the commits and push it
+
+git checkout --orphan "$NEW_BRANCH_NAME"
+git reset .
+git clean -fd
+git cherry-pick "$BUGGY_COMMIT_ID"
+git cherry-pick "$TEST_COMMIT_ID"
+git cherry-pick "$PATCHED_COMMIT_ID"
+git cherry-pick "$END_COMMIT_ID"
+git push github "$NEW_BRANCH_NAME":"$NEW_BRANCH_NAME"
+
+# update bears.json in such new branch
+
+git checkout pr-add-bug
+python ./.travis_bears/add_bug/update_bears_json_file.py "$NEW_BRANCH_NAME"
---
+++ .travis_bears/check_pr/check_with_maven/check_with_maven.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ./check_with_maven.sh <branch name> <1 for buggy commit, 0 for patched commit>"
+    exit 2
+fi
+
+BRANCH_NAME=$1
+IS_BUGGY_COMMIT=$2
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+MAVEN_TEST_ARGS="-Denforcer.skip=true -Dcheckstyle.skip=true -Dcobertura.skip=true -DskipITs=true -Drat.skip=true -Dlicense.skip=true -Dfindbugs.skip=true -Dgpg.skip=true -Dskip.npm=true -Dskip.gulp=true -Dskip.bower=true"
+
+cd pr
+
+BUGGY_COMMIT_ID=""
+
+CASE=$(cat bears.json | sed 's/.*"type": "\(.*\)".*/\1/;t;d')
+echo "> Branch from case $CASE"
+if [ "$CASE" == "failing_passing" ]; then
+    BUGGY_COMMIT_ID=$(git log --format=format:%H --grep="Bug commit")
+else
+    BUGGY_COMMIT_ID=$(git log --format=format:%H --grep="Changes in the tests")
+fi
+
+PATCHED_COMMIT_ID=$(git log --format=format:%H --grep="Human patch")
+
+if [ "$IS_BUGGY_COMMIT" -eq 1 ]; then
+
+    echo "> Checking out the buggy commit: $BUGGY_COMMIT_ID"
+    git log --format=%B -n 1 $BUGGY_COMMIT_ID
+
+    git checkout -q $BUGGY_COMMIT_ID
+
+    mvn install -V -DskipTests=true -B  $MAVEN_TEST_ARGS
+
+    mvn -B test $MAVEN_TEST_ARGS
+
+    status=$?
+    if [ "$status" -eq 0 ]; then
+        echo -e "$RED$BRANCH_NAME [FAILURE] (bug reproduction - status = 0)"
+        exit 1
+    fi
+
+else
+
+    echo "> Checking out the patched commit: $PATCHED_COMMIT_ID"
+    git log --format=%B -n 1 $PATCHED_COMMIT_ID
+
+    git checkout -q $PATCHED_COMMIT_ID
+
+    mvn install -V -DskipTests=true -B  $MAVEN_TEST_ARGS
+
+    mvn -B test $MAVEN_TEST_ARGS
+
+    status=$?
+    if [ "$status" -ne 0 ]; then
+        echo -e "$RED$BRANCH_NAME [FAILURE] (patch reproduction - status = $status)"
+        exit 1
+    fi
+fi
+
+echo -e "$GREEN$BRANCH_NAME [OK]"
