ba2f08600a5007169c0da141a8e0861cb0796b8d
fermadeiral <fer.madeiral@gmail.com>
2018-11-07 07:39:02 +0100

    Changes in the tests This commit is based on the source code from the following commit: http://github.com/org-tigris-jsapar/jsapar/commit/97bde7f2f5051e8a408133c75e38691e96ea7a97 The mentioned commit triggered the following Travis build: http://travis-ci.org/org-tigris-jsapar/jsapar/builds/451741055.

--- src/test/java/org/jsapar/parse/bean/Bean2CellTest.java
+++ src/test/java/org/jsapar/parse/bean/Bean2CellTest.java
@@ -4,6 +4,7 @@ import org.jsapar.TstPerson;
 import org.jsapar.compose.bean.BeanComposeException;
 import org.jsapar.model.BooleanCell;
 import org.jsapar.model.Cell;
+import org.jsapar.model.StringCell;
 import org.junit.Test;
 
 import java.beans.BeanInfo;
@@ -30,10 +31,7 @@ public class Bean2CellTest {
 
     @Test
     public void makeCell() throws IntrospectionException, InvocationTargetException, IllegalAccessException {
-        BeanInfo beanInfo = Introspector.getBeanInfo(TstPerson.class);
-        PropertyDescriptor propertyDescriptor = Arrays.stream(beanInfo.getPropertyDescriptors())
-                .filter(it -> it.getName().equals("adult")).findFirst().orElseThrow(AssertionError::new);
-        Bean2Cell bean2Cell = Bean2Cell.ofCellName("adult", propertyDescriptor);
+        Bean2Cell bean2Cell = makeBean2CellOfPropertyName(TstPerson.class, "adult");
         TstPerson tstPerson = new TstPerson();
         tstPerson.setAdult(true);
         Cell cell = bean2Cell.makeCell(tstPerson);
@@ -47,10 +45,7 @@ public class Bean2CellTest {
     public void assign_Boolean()
             throws IntrospectionException, InvocationTargetException, InstantiationException, IllegalAccessException,
             BeanComposeException {
-        BeanInfo beanInfo = Introspector.getBeanInfo(TstPerson.class);
-        PropertyDescriptor propertyDescriptor = Arrays.stream(beanInfo.getPropertyDescriptors())
-                .filter(it -> it.getName().equals("adult")).findFirst().orElseThrow(AssertionError::new);
-        Bean2Cell bean2Cell = Bean2Cell.ofCellName("adult", propertyDescriptor);
+        Bean2Cell bean2Cell = makeBean2CellOfPropertyName(TstPerson.class, "adult");
         TstPerson tstPerson = new TstPerson();
         assertFalse(tstPerson.isAdult());
         bean2Cell.assign(tstPerson, new BooleanCell("adult", true));
@@ -58,4 +53,35 @@ public class Bean2CellTest {
         bean2Cell.assign(tstPerson, new BooleanCell("adult", false));
         assertFalse(tstPerson.isAdult());
     }
+
+    @Test
+    public void assign_String()
+            throws IntrospectionException, InvocationTargetException, InstantiationException, IllegalAccessException,
+            BeanComposeException {
+        Bean2Cell bean2Cell = makeBean2CellOfPropertyName(TstPerson.class, "lastName");
+        TstPerson tstPerson = new TstPerson();
+        assertEquals("Nobody", tstPerson.getLastName());
+        bean2Cell.assign(tstPerson, new StringCell("lastName", "Somebody"));
+        assertEquals("Somebody", tstPerson.getLastName());
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private Bean2Cell makeBean2CellOfPropertyName(Class<?> beanClass, String propertyName) throws IntrospectionException {
+        BeanInfo beanInfo = Introspector.getBeanInfo(beanClass);
+        PropertyDescriptor propertyDescriptor = Arrays.stream(beanInfo.getPropertyDescriptors())
+                .filter(it -> it.getName().equals(propertyName)).findFirst().orElseThrow(AssertionError::new);
+        return Bean2Cell.ofCellName(propertyName, propertyDescriptor);
+    }
+
+    @Test
+    public void assign_empty()
+            throws IntrospectionException, InvocationTargetException, InstantiationException, IllegalAccessException,
+            BeanComposeException {
+        Bean2Cell bean2Cell = makeBean2CellOfPropertyName(TstPerson.class, "lastName");
+        TstPerson tstPerson = new TstPerson();
+        assertEquals("Nobody", tstPerson.getLastName());
+        bean2Cell.assign(tstPerson, StringCell.emptyOf("lastName"));
+        assertEquals("Nobody", tstPerson.getLastName());
+    }
+
 }
