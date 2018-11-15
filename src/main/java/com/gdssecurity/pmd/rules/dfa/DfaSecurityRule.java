be72077c91432d372694636349f7b5ddebe8b3c2
fermadeiral <fer.madeiral@gmail.com>
2018-11-15 21:12:51 +0100

    Human patch from albfernandez/GDS-PMD-Security-Rules This commit is based on the source code from the following commit: http://github.com/albfernandez/GDS-PMD-Security-Rules/commit/d08d1b9f55598fca9b5a9b78b89998f81edeed7c The mentioned commit triggered the following Travis build: http://travis-ci.org/albfernandez/GDS-PMD-Security-Rules/builds/455669767.

--- src/main/java/com/gdssecurity/pmd/rules/dfa/DfaSecurityRule.java
+++ src/main/java/com/gdssecurity/pmd/rules/dfa/DfaSecurityRule.java
@@ -544,6 +544,9 @@ public class DfaSecurityRule extends BaseSecurityRule implements Executable {
 	private boolean isAnyArgumentTainted(Node simpleNode) {
 		ASTArgumentList argListNode = simpleNode.getFirstDescendantOfType(ASTArgumentList.class);
 		if (argListNode != null) {
+			if (isSanitized(argListNode)) {
+				return false;
+			}
 			int numChildren = argListNode.jjtGetNumChildren();
 			for (int i = 0; i < numChildren; i++) {
 				Node argument = argListNode.jjtGetChild(i);
@@ -551,6 +554,27 @@ public class DfaSecurityRule extends BaseSecurityRule implements Executable {
 					return true;
 				}
 			}
+		}
+		return false;
+	}
+
+	private boolean isSanitized(ASTArgumentList argListNode) {
+		try {
+			ASTArguments arguments = (ASTArguments) argListNode.jjtGetParent();
+			ASTPrimarySuffix suffix = (ASTPrimarySuffix) arguments.jjtGetParent();
+			ASTPrimaryExpression exp = (ASTPrimaryExpression) suffix.jjtGetParent();
+			String method = getMethod(exp);
+			Class<?> type = getJavaType(exp);
+			String typeName = type.getName();
+			if (isSink(typeName, method)) {
+				return false;
+			}
+			if (isSanitizerMethod(typeName, method) || isSafeType(getReturnType(exp, typeName, method))) {
+				return true;
+			}
+		}
+		catch (Exception e) {
+			return false;
 		}
 		return false;
 	}
