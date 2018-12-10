1593380194542245735587c8fab7fb07e2b6cc26
fermadeiral <fer.madeiral@gmail.com>
2018-12-11 00:43:39 +0100

    Bug commit from spring-projects/spring-data-commons This commit is based on the source code from the following commit: http://github.com/spring-projects/spring-data-commons/commit/6199fed1c13d228f8a6c8c479f6273f869261f96 The mentioned commit triggered the following Travis build: http://travis-ci.org/spring-projects/spring-data-commons/builds/303079759.

---
+++ src/main/java/org/springframework/data/domain/Pageable.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2008-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.domain;
+
+/**
+ * Abstract interface for pagination information.
+ * 
+ * @author Oliver Gierke
+ */
+public interface Pageable {
+
+	/**
+	 * Returns the page to be returned.
+	 * 
+	 * @return the page to be returned.
+	 */
+	int getPageNumber();
+
+	/**
+	 * Returns the number of items to be returned.
+	 * 
+	 * @return the number of items of that page
+	 */
+	int getPageSize();
+
+	/**
+	 * Returns the offset to be taken according to the underlying page and page size.
+	 * 
+	 * @return the offset to be taken
+	 */
+	int getOffset();
+
+	/**
+	 * Returns the sorting parameters.
+	 * 
+	 * @return
+	 */
+	Sort getSort();
+
+	/**
+	 * Returns the {@link Pageable} requesting the next {@link Page}.
+	 * 
+	 * @return
+	 */
+	Pageable next();
+
+	/**
+	 * Returns the previous {@link Pageable} or the first {@link Pageable} if the current one already is the first one.
+	 * 
+	 * @return
+	 */
+	Pageable previousOrFirst();
+
+	/**
+	 * Returns the {@link Pageable} requesting the first page.
+	 * 
+	 * @return
+	 */
+	Pageable first();
+
+	/**
+	 * Returns whether there's a previous {@link Pageable} we can access from the current one. Will return
+	 * {@literal false} in case the current {@link Pageable} already refers to the first page.
+	 * 
+	 * @return
+	 */
+	boolean hasPrevious();
+}
---
+++ src/main/java/org/springframework/data/web/config/SpringDataWebConfiguration.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2013-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.web.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.data.geo.format.DistanceFormatter;
+import org.springframework.data.geo.format.PointFormatter;
+import org.springframework.data.repository.support.DomainClassConverter;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.ProjectingJackson2HttpMessageConverter;
+import org.springframework.data.web.ProxyingHandlerMethodArgumentResolver;
+import org.springframework.data.web.SortHandlerMethodArgumentResolver;
+import org.springframework.data.web.XmlBeamHttpMessageConverter;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.support.FormattingConversionService;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.util.ClassUtils;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Configuration class to register {@link PageableHandlerMethodArgumentResolver},
+ * {@link SortHandlerMethodArgumentResolver} and {@link DomainClassConverter}.
+ *
+ * @since 1.6
+ * @author Oliver Gierke
+ * @author Jens Schauder
+ */
+@Configuration
+public class SpringDataWebConfiguration extends WebMvcConfigurerAdapter {
+
+	@Autowired private ApplicationContext context;
+	@Autowired @Qualifier("mvcConversionService") ObjectFactory<ConversionService> conversionService;
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.web.config.SpringDataWebConfiguration#pageableResolver()
+	 */
+	@Bean
+	public PageableHandlerMethodArgumentResolver pageableResolver() {
+		return new PageableHandlerMethodArgumentResolver(sortResolver());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.web.config.SpringDataWebConfiguration#sortResolver()
+	 */
+	@Bean
+	public SortHandlerMethodArgumentResolver sortResolver() {
+		return new SortHandlerMethodArgumentResolver();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter#addFormatters(org.springframework.format.FormatterRegistry)
+	 */
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+
+		registry.addFormatter(DistanceFormatter.INSTANCE);
+		registry.addFormatter(PointFormatter.INSTANCE);
+
+		if (!(registry instanceof FormattingConversionService)) {
+			return;
+		}
+
+		FormattingConversionService conversionService = (FormattingConversionService) registry;
+
+		DomainClassConverter<FormattingConversionService> converter = new DomainClassConverter<FormattingConversionService>(
+				conversionService);
+		converter.setApplicationContext(context);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter#addArgumentResolvers(java.util.List)
+	 */
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+
+		argumentResolvers.add(sortResolver());
+		argumentResolvers.add(pageableResolver());
+
+		ProxyingHandlerMethodArgumentResolver resolver = new ProxyingHandlerMethodArgumentResolver(
+				conversionService.getObject());
+		resolver.setBeanFactory(context);
+		resolver.setBeanClassLoader(context.getClassLoader());
+
+		argumentResolvers.add(resolver);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter#extendMessageConverters(java.util.List)
+	 */
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+		if (ClassUtils.isPresent("com.jayway.jsonpath.DocumentContext", context.getClassLoader())
+				&& ClassUtils.isPresent("com.fasterxml.jackson.databind.ObjectMapper", context.getClassLoader())) {
+
+			ObjectMapper mapper = getUniqueBean(ObjectMapper.class, context);
+			mapper = mapper == null ? new ObjectMapper() : mapper;
+
+			ProjectingJackson2HttpMessageConverter converter = new ProjectingJackson2HttpMessageConverter(mapper);
+			converter.setBeanClassLoader(context.getClassLoader());
+			converter.setBeanFactory(context);
+
+			converters.add(0, converter);
+		}
+
+		if (ClassUtils.isPresent("org.xmlbeam.XBProjector", context.getClassLoader())) {
+			converters.add(0, new XmlBeamHttpMessageConverter());
+		}
+	}
+
+	private static <T> T getUniqueBean(Class<T> type, ApplicationContext context) {
+
+		try {
+			return context.getBean(type);
+		} catch (NoSuchBeanDefinitionException o_O) {
+			return null;
+		}
+	}
+}
