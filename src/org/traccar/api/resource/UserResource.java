a0bf6c8d78f8464500e7671c5e2d75b2da1573ea
fermadeiral <fer.madeiral@gmail.com>
2018-12-10 13:06:57 +0100

    Bug commit from traccar/traccar This commit is based on the source code from the following commit: http://github.com/traccar/traccar/commit/9e9f97ac34d882594e3d23e959775ce765447787 The mentioned commit triggered the following Travis build: http://travis-ci.org/traccar/traccar/builds/265439859.

---
+++ src/org/traccar/api/resource/UserResource.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2015 - 2017 Anton Tananaev (anton@traccar.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.api.resource;
+
+import org.traccar.Context;
+import org.traccar.api.BaseObjectResource;
+import org.traccar.database.UsersManager;
+import org.traccar.model.ManagedUser;
+import org.traccar.model.User;
+
+import javax.annotation.security.PermitAll;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Set;
+
+@Path("users")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserResource extends BaseObjectResource<User> {
+
+    public UserResource() {
+        super(User.class);
+    }
+
+    @GET
+    public Collection<User> get(@QueryParam("userId") long userId) throws SQLException {
+        UsersManager usersManager = Context.getUsersManager();
+        Set<Long> result = null;
+        if (Context.getPermissionsManager().isAdmin(getUserId())) {
+            if (userId != 0) {
+                result = usersManager.getUserItems(userId);
+            } else {
+                result = usersManager.getAllItems();
+            }
+        } else if (Context.getPermissionsManager().isManager(getUserId())) {
+            result = usersManager.getManagedItems(getUserId());
+        } else {
+            throw new SecurityException("Admin or manager access required");
+        }
+        return usersManager.getItems(result);
+    }
+
+    @Override
+    @PermitAll
+    @POST
+    public Response add(User entity) throws SQLException {
+        if (!Context.getPermissionsManager().isAdmin(getUserId())) {
+            Context.getPermissionsManager().checkUserUpdate(getUserId(), new User(), entity);
+            if (Context.getPermissionsManager().isManager(getUserId())) {
+                Context.getPermissionsManager().checkUserLimit(getUserId());
+            } else {
+                Context.getPermissionsManager().checkRegistration(getUserId());
+                entity.setDeviceLimit(Context.getConfig().getInteger("users.defaultDeviceLimit", -1));
+                int expirationDays = Context.getConfig().getInteger("users.defaultExpirationDays");
+                if (expirationDays > 0) {
+                    entity.setExpirationTime(
+                        new Date(System.currentTimeMillis() + (long) expirationDays * 24 * 3600 * 1000));
+                }
+            }
+        }
+        Context.getUsersManager().addItem(entity);
+        if (Context.getPermissionsManager().isManager(getUserId())) {
+            Context.getDataManager().linkObject(User.class, getUserId(), ManagedUser.class, entity.getId(), true);
+        }
+        Context.getUsersManager().refreshUserItems();
+        if (Context.getNotificationManager() != null) {
+            Context.getNotificationManager().refresh();
+        }
+        return Response.ok(entity).build();
+    }
+
+}
