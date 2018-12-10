d09bad016a52867918c00a2eb417ceec0d9b9b72
fermadeiral <fer.madeiral@gmail.com>
2018-12-10 02:21:20 +0100

    Bug commit from FasterXML/jackson-databind This commit is based on the source code from the following commit: http://github.com/FasterXML/jackson-databind/commit/fe80e86551be38fccb1a983854925a78db6e845f The mentioned commit triggered the following Travis build: http://travis-ci.org/FasterXML/jackson-databind/builds/242268513.

---
+++ src/main/java/com/fasterxml/jackson/databind/introspect/ConcreteBeanPropertyBase.java
@@ -0,0 +1,110 @@
+package com.fasterxml.jackson.databind.introspect;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.AnnotationIntrospector;
+import com.fasterxml.jackson.databind.BeanProperty;
+import com.fasterxml.jackson.databind.PropertyMetadata;
+import com.fasterxml.jackson.databind.cfg.MapperConfig;
+
+/**
+ * Intermediate {@link BeanProperty} class shared by concrete readable- and
+ * writable property implementations for sharing common functionality.
+ *
+ * @since 2.7
+ */
+public abstract class ConcreteBeanPropertyBase
+    implements BeanProperty, java.io.Serializable
+{
+    private static final long serialVersionUID = 1;
+
+    /**
+     * Additional information about property
+     *
+     * @since 2.3
+     */
+    protected final PropertyMetadata _metadata;
+    
+    /**
+     * Lazily accessed value for per-property format override definition.
+     * 
+     * @since 2.8
+     */
+    protected transient JsonFormat.Value _propertyFormat;
+
+    protected ConcreteBeanPropertyBase(PropertyMetadata md) {
+        _metadata = (md == null) ? PropertyMetadata.STD_REQUIRED_OR_OPTIONAL : md;
+    }
+
+    protected ConcreteBeanPropertyBase(ConcreteBeanPropertyBase src) {
+        _metadata = src._metadata;
+        _propertyFormat = src._propertyFormat;
+    }
+
+    @Override
+    public boolean isRequired() { return _metadata.isRequired(); }
+
+    @Override
+    public PropertyMetadata getMetadata() { return _metadata; }
+    
+    @Override
+    public boolean isVirtual() { return false; }
+
+    @Override
+    @Deprecated
+    public final JsonFormat.Value findFormatOverrides(AnnotationIntrospector intr) {
+        JsonFormat.Value f = null;
+        if (intr != null) {
+            AnnotatedMember member = getMember();
+            if (member != null) {
+                f = intr.findFormat(member);
+            }
+        }
+        if (f == null) {
+            f = EMPTY_FORMAT;
+        }
+        return f;
+    }
+
+    @Override
+    public JsonFormat.Value findPropertyFormat(MapperConfig<?> config, Class<?> baseType)
+    {
+        // 15-Apr-2016, tatu: Let's calculate lazily, retain; assumption being however that
+        //    baseType is always the same
+        JsonFormat.Value v = _propertyFormat;
+        if (v == null) {
+            JsonFormat.Value v1 = config.getDefaultPropertyFormat(baseType);
+            JsonFormat.Value v2 = null;
+            AnnotationIntrospector intr = config.getAnnotationIntrospector();
+            if (intr != null) {
+                AnnotatedMember member = getMember();
+                if (member != null) {
+                    v2 = intr.findFormat(member);
+                }
+            }
+            if (v1 == null) {
+                v = (v2 == null) ? EMPTY_FORMAT : v2;
+            } else {
+                v = (v2 == null) ? v1 : v1.withOverrides(v2);
+            }
+            _propertyFormat = v;
+        }
+        return v;
+    }
+
+    @Override
+    public JsonInclude.Value findPropertyInclusion(MapperConfig<?> config, Class<?> baseType)
+    {
+        JsonInclude.Value v0 = config.getDefaultPropertyInclusion(baseType);
+        AnnotationIntrospector intr = config.getAnnotationIntrospector();
+        AnnotatedMember member = getMember();
+        if ((intr == null) || (member == null)) {
+            return v0;
+        }
+        JsonInclude.Value v = intr.findPropertyInclusion(member);
+        if (v == null) {
+            return v0;
+        }
+        return v0.withOverrides(v);
+    }
+}
---
+++ src/test/java/com/fasterxml/jackson/databind/creators/ImplicitParamsForCreatorTest.java
@@ -0,0 +1,49 @@
+package com.fasterxml.jackson.databind.creators;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.introspect.AnnotatedMember;
+import com.fasterxml.jackson.databind.introspect.AnnotatedParameter;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+
+public class ImplicitParamsForCreatorTest extends BaseMapTest
+{
+    @SuppressWarnings("serial")
+    static class MyParamIntrospector extends JacksonAnnotationIntrospector
+    {
+        @Override
+        public String findImplicitPropertyName(AnnotatedMember param) {
+            if (param instanceof AnnotatedParameter) {
+                AnnotatedParameter ap = (AnnotatedParameter) param;
+                return "paramName"+ap.getIndex();
+            }
+            return super.findImplicitPropertyName(param);
+        }
+    }
+
+    static class XY {
+        protected int x, y;
+
+        // annotation should NOT be needed with 2.6 any more (except for single-arg case)
+        //@com.fasterxml.jackson.annotation.JsonCreator
+        public XY(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    /*
+    /**********************************************************
+    /* Test methods
+    /**********************************************************
+     */
+
+    public void testNonSingleArgCreator() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setAnnotationIntrospector(new MyParamIntrospector());
+        XY value = mapper.readValue(aposToQuotes("{'paramName0':1,'paramName1':2}"), XY.class);
+        assertNotNull(value);
+        assertEquals(1, value.x);
+        assertEquals(2, value.y);
+    }
+}
