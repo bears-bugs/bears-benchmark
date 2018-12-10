5cbd4cf2a3e5627aa670fbf4c4970db87b8b942a
fermadeiral <fer.madeiral@gmail.com>
2018-12-10 01:55:26 +0100

    Bug commit from traccar/traccar This commit is based on the source code from the following commit: http://github.com/traccar/traccar/commit/2cf20c46afe59f6e98e629a6c88e74d7ab60b122 The mentioned commit triggered the following Travis build: http://travis-ci.org/traccar/traccar/builds/248584369.

---
+++ test/org/traccar/events/MotionEventHandlerTest.java
@@ -0,0 +1,29 @@
+package org.traccar.events;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Collection;
+
+import org.junit.Test;
+import org.traccar.BaseTest;
+import org.traccar.model.Event;
+import org.traccar.model.Position;
+
+public class MotionEventHandlerTest extends BaseTest {
+    
+    @Test
+    public void testMotionEventHandler() throws Exception {
+        
+        MotionEventHandler motionEventHandler = new MotionEventHandler();
+        
+        Position position = new Position();
+        position.set(Position.KEY_MOTION, true);
+        position.setValid(true);
+        Collection<Event> events = motionEventHandler.analyzePosition(position);
+        assertNotNull(events);
+        Event event = (Event) events.toArray()[0];
+        assertEquals(Event.TYPE_DEVICE_MOVING, event.getType());
+    }
+
+}
