3d7ebad544406bff5bc425318f9bfbf7dd4b3510
fermadeiral <fer.madeiral@gmail.com>
2018-12-10 12:30:29 +0100

    Bug commit from traccar/traccar This commit is based on the source code from the following commit: http://github.com/traccar/traccar/commit/df53571ab09afb15888462dbbed2c3d58759c55a The mentioned commit triggered the following Travis build: http://travis-ci.org/traccar/traccar/builds/255051210.

---
+++ test/org/traccar/protocol/Pt502ProtocolEncoderTest.java
@@ -0,0 +1,54 @@
+package org.traccar.protocol;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.traccar.ProtocolTest;
+import org.traccar.model.Command;
+
+public class Pt502ProtocolEncoderTest extends ProtocolTest {
+
+    @Test
+    public void testEncodeOutputControl() throws Exception {
+
+        Pt502ProtocolEncoder encoder = new Pt502ProtocolEncoder();
+        
+        Command command = new Command();
+        command.setDeviceId(1);
+        command.setType(Command.TYPE_OUTPUT_CONTROL);
+        command.set(Command.KEY_INDEX, 2);
+        command.set(Command.KEY_DATA, 1);
+        
+        Assert.assertEquals("#OPC2,1\r\n", encoder.encodeCommand(command));
+
+    }
+    
+    @Test
+    public void testEncodeTimezone() throws Exception {
+
+        Pt502ProtocolEncoder encoder = new Pt502ProtocolEncoder();
+
+        Command command = new Command();
+        command.setDeviceId(1);
+        command.setType(Command.TYPE_SET_TIMEZONE);
+        command.set(Command.KEY_TIMEZONE, 8);
+
+        Assert.assertEquals("#TMZ8\r\n", encoder.encodeCommand(command));
+
+    }
+
+
+    @Test
+    public void testEncodeAlarmSpeed() throws Exception {
+
+        Pt502ProtocolEncoder encoder = new Pt502ProtocolEncoder();
+
+        Command command = new Command();
+        command.setDeviceId(1);
+        command.setType(Command.TYPE_ALARM_SPEED);
+        command.set(Command.KEY_DATA, 120);
+
+        Assert.assertEquals("#SPD120\r\n", encoder.encodeCommand(command));
+
+    }
+
+}
