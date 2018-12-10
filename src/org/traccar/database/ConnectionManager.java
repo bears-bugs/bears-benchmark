691021cdddea6204cfd3fe9d64f0d4f74407da46
fermadeiral <fer.madeiral@gmail.com>
2018-12-10 13:10:47 +0100

    Bug commit from traccar/traccar This commit is based on the source code from the following commit: http://github.com/traccar/traccar/commit/b82be4f9f3b4d1d6c345bc41d5c26aca03e369cb The mentioned commit triggered the following Travis build: http://travis-ci.org/traccar/traccar/builds/262940348.

---
+++ src/org/traccar/database/ConnectionManager.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright 2015 - 2017 Anton Tananaev (anton@traccar.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.database;
+
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.util.Timeout;
+import org.jboss.netty.util.TimerTask;
+import org.traccar.Context;
+import org.traccar.GlobalTimer;
+import org.traccar.Protocol;
+import org.traccar.helper.Log;
+import org.traccar.model.Device;
+import org.traccar.model.DeviceState;
+import org.traccar.model.Event;
+import org.traccar.model.Position;
+import org.traccar.reports.ReportUtils;
+import org.traccar.reports.model.TripsConfig;
+
+import java.net.SocketAddress;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+public class ConnectionManager {
+
+    private static final long DEFAULT_TIMEOUT = 600;
+
+    private final long deviceTimeout;
+    private final boolean enableStatusEvents;
+    private TripsConfig tripsConfig = null;
+
+    private final Map<Long, ActiveDevice> activeDevices = new ConcurrentHashMap<>();
+    private final Map<Long, Set<UpdateListener>> listeners = new ConcurrentHashMap<>();
+    private final Map<Long, Timeout> timeouts = new ConcurrentHashMap<>();
+
+    public ConnectionManager() {
+        deviceTimeout = Context.getConfig().getLong("status.timeout", DEFAULT_TIMEOUT) * 1000;
+        enableStatusEvents = Context.getConfig().getBoolean("event.enable");
+        if (Context.getConfig().getBoolean("status.updateDeviceState")) {
+            tripsConfig = ReportUtils.initTripsConfig();
+        }
+    }
+
+    public void addActiveDevice(long deviceId, Protocol protocol, Channel channel, SocketAddress remoteAddress) {
+        activeDevices.put(deviceId, new ActiveDevice(deviceId, protocol, channel, remoteAddress));
+    }
+
+    public void removeActiveDevice(Channel channel) {
+        for (ActiveDevice activeDevice : activeDevices.values()) {
+            if (activeDevice.getChannel() == channel) {
+                updateDevice(activeDevice.getDeviceId(), Device.STATUS_OFFLINE, null);
+                activeDevices.remove(activeDevice.getDeviceId());
+                break;
+            }
+        }
+    }
+
+    public ActiveDevice getActiveDevice(long deviceId) {
+        return activeDevices.get(deviceId);
+    }
+
+    public void updateDevice(final long deviceId, String status, Date time) {
+        Device device = Context.getIdentityManager().getById(deviceId);
+        if (device == null) {
+            return;
+        }
+
+        String oldStatus = device.getStatus();
+        device.setStatus(status);
+
+        if (enableStatusEvents && !status.equals(oldStatus)) {
+            String eventType;
+            Event stateEvent = null;
+            switch (status) {
+                case Device.STATUS_ONLINE:
+                    eventType = Event.TYPE_DEVICE_ONLINE;
+                    break;
+                case Device.STATUS_UNKNOWN:
+                    eventType = Event.TYPE_DEVICE_UNKNOWN;
+                    if (tripsConfig != null) {
+                        stateEvent = updateDeviceState(deviceId);
+                    }
+                    break;
+                default:
+                    eventType = Event.TYPE_DEVICE_OFFLINE;
+                    if (tripsConfig != null) {
+                        stateEvent = updateDeviceState(deviceId);
+                    }
+                    break;
+            }
+            Set<Event> events = new HashSet<>();
+            if (stateEvent != null) {
+                events.add(stateEvent);
+            }
+            events.add(new Event(eventType, deviceId));
+            Context.getNotificationManager().updateEvents(events, null);
+        }
+
+        Timeout timeout = timeouts.remove(deviceId);
+        if (timeout != null) {
+            timeout.cancel();
+        }
+
+        if (time != null) {
+            device.setLastUpdate(time);
+        }
+
+        if (status.equals(Device.STATUS_ONLINE)) {
+            timeouts.put(deviceId, GlobalTimer.getTimer().newTimeout(new TimerTask() {
+                @Override
+                public void run(Timeout timeout) throws Exception {
+                    if (!timeout.isCancelled()) {
+                        updateDevice(deviceId, Device.STATUS_UNKNOWN, null);
+                    }
+                }
+            }, deviceTimeout, TimeUnit.MILLISECONDS));
+        }
+
+        try {
+            Context.getDeviceManager().updateDeviceStatus(device);
+        } catch (SQLException error) {
+            Log.warning(error);
+        }
+
+        updateDevice(device);
+    }
+
+    public Event updateDeviceState(long deviceId) {
+        DeviceState deviceState = Context.getDeviceManager().getDeviceState(deviceId);
+        if (deviceState == null || deviceState.getMotionState() == null) {
+            return null;
+        }
+        Event result = null;
+        Boolean oldMotion = deviceState.getMotionState();
+        long currentTime = System.currentTimeMillis();
+        boolean newMotion = !oldMotion;
+        Position motionPosition = deviceState.getMotionPosition();
+        if (motionPosition != null) {
+            long motionTime = motionPosition.getFixTime().getTime()
+                    + (newMotion ? tripsConfig.getMinimalTripDuration() : tripsConfig.getMinimalParkingDuration());
+            if (motionTime <= currentTime) {
+                String eventType = newMotion ? Event.TYPE_DEVICE_MOVING : Event.TYPE_DEVICE_STOPPED;
+                result = new Event(eventType, motionPosition.getDeviceId(), motionPosition.getId());
+                deviceState.setMotionState(newMotion);
+                deviceState.setMotionPosition(null);
+            }
+        }
+        return result;
+    }
+
+    public synchronized void updateDevice(Device device) {
+        for (long userId : Context.getPermissionsManager().getDeviceUsers(device.getId())) {
+            if (listeners.containsKey(userId)) {
+                for (UpdateListener listener : listeners.get(userId)) {
+                    listener.onUpdateDevice(device);
+                }
+            }
+        }
+    }
+
+    public synchronized void updatePosition(Position position) {
+        long deviceId = position.getDeviceId();
+
+        for (long userId : Context.getPermissionsManager().getDeviceUsers(deviceId)) {
+            if (listeners.containsKey(userId)) {
+                for (UpdateListener listener : listeners.get(userId)) {
+                    listener.onUpdatePosition(position);
+                }
+            }
+        }
+    }
+
+    public synchronized void updateEvent(long userId, Event event) {
+        if (listeners.containsKey(userId)) {
+            for (UpdateListener listener : listeners.get(userId)) {
+                listener.onUpdateEvent(event);
+            }
+        }
+    }
+
+    public interface UpdateListener {
+        void onUpdateDevice(Device device);
+        void onUpdatePosition(Position position);
+        void onUpdateEvent(Event event);
+    }
+
+    public synchronized void addListener(long userId, UpdateListener listener) {
+        if (!listeners.containsKey(userId)) {
+            listeners.put(userId, new HashSet<UpdateListener>());
+        }
+        listeners.get(userId).add(listener);
+    }
+
+    public synchronized void removeListener(long userId, UpdateListener listener) {
+        if (!listeners.containsKey(userId)) {
+            listeners.put(userId, new HashSet<UpdateListener>());
+        }
+        listeners.get(userId).remove(listener);
+    }
+
+}
