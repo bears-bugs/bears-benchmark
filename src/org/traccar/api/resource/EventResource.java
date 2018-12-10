7b6783b529426460c41859d9f39b09a4cc6117d4
fermadeiral <fer.madeiral@gmail.com>
2018-12-10 23:49:46 +0100

    Bug commit from traccar/traccar This commit is based on the source code from the following commit: http://github.com/traccar/traccar/commit/0adacee61b3ddaacdbac7f47773e9dcbcf5b083c The mentioned commit triggered the following Travis build: http://travis-ci.org/traccar/traccar/builds/308288525.

---
+++ src/org/traccar/api/resource/EventResource.java
@@ -0,0 +1,34 @@
+package org.traccar.api.resource;
+
+import java.sql.SQLException;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.traccar.Context;
+import org.traccar.api.BaseResource;
+import org.traccar.model.Event;
+import org.traccar.model.Geofence;
+
+@Path("events")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+
+public class EventResource extends BaseResource {
+
+    @Path("{id}")
+    @GET
+    public Event get(@PathParam("id") long id) throws SQLException {
+        Event event = Context.getDataManager().getObject(Event.class, id);
+        Context.getPermissionsManager().checkDevice(getUserId(), event.getDeviceId());
+        if (event.getGeofenceId() != 0) {
+            Context.getPermissionsManager().checkPermission(Geofence.class, getUserId(), event.getGeofenceId());
+        }
+        return event;
+    }
+
+}
